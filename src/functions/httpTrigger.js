cc7974460f2d22a3cea740c58d82e6c3d806eefc
Kirlitos <rodrigoapsalvado@gmail.com>
2025-04-08 00:44:32 +0100

    update

--- src/functions/httpTrigger.js
+++ src/functions/httpTrigger.js
@@ -3,5 +3,5 @@ const { app } = require('@azure/functions');
 app.http('httpTrigger', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: require('./functions/httpTrigger')
+    handler: require('./functions/APIreddit.js')
 });
