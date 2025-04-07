e8897ad6e93c1c31904f19ebfe4531e31bc90ed8
Kirlitos <rodrigoapsalvado@gmail.com>
2025-04-08 00:44:00 +0100

    update

--- src/functions/httpTrigger.js
+++ src/functions/httpTrigger.js
@@ -3,11 +3,5 @@ const { app } = require('@azure/functions');
 app.http('httpTrigger', {
     methods: ['GET', 'POST'],
     authLevel: 'anonymous',
-    handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
-
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
-    }
+    handler: require('./functions/httpTrigger')
 });
